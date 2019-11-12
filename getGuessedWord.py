0284627c963d14c118bf6d4e4c800dc2047bc79e
JohnEmmanuelReyes <47804220+JohnEmmanuelReyes@users.noreply.github.com>
2019-11-12 20:54:32 +0800

    Delete getGuessedWord.py

--- getGuessedWord.py
+++
@@ -1,9 +0,0 @@
-def isWordGuessed(secretWord, lettersGuessed):
-  w=secretWord
-  if w.isalpha()==True:
-      for letter in secretWord:
-            if letter not in lettersGuessed:
-              w=w.replace(letter,"_ ")
-      return w
-  else:
-      print("INVALID!!:Input only letters")
