c4d2d4792735faabd994f6437e9e7c941af496d3
JohnEmmanuelReyes <47804220+JohnEmmanuelReyes@users.noreply.github.com>
2019-11-12 20:44:16 +0800

    Add files via upload

---
+++ getGuessedWord.py
@@ -0,0 +1,9 @@
+def isWordGuessed(secretWord, lettersGuessed):
+  w=secretWord
+  if w.isalpha()==True:
+      for letter in secretWord:
+            if letter not in lettersGuessed:
+              w=w.replace(letter,"_ ")
+      return w
+  else:
+      print("INVALID!!:Input only letters")
